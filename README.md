3f056407db9ab9bbc0b67c835a3da13b5be9d985
Ernesto Canquiz <fdsoil123@gmail.com>
2020-07-31 16:04:39 -0400

    Lesson 2 - Laravel-mix, NPM, Package.json, Composer
    Lesson 1 - Presentation - VUEjs component course in Laravel

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # php_laravel_rimorsoft_vl_vue
 Php Laravel Rimorsoft VL Vue
+
+Lesson 1 - Presentation - VUEjs component course in Laravel
+
+Lesson 2 - Laravel-mix, NPM, Package.json, Composer
+
+To create the model with migration:
+
+$ php artisan make:model Idea -m
+
+$ npm install
+
+$ npm run dev
+
+Because we delete everything related to User, then we must execute:
+
+$ composer dump-autoload 
+
+$ php artisan migrate:refresh
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel - Components VUEjs</title>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">    
+    </head>
+    <body>
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
